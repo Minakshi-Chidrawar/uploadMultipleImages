13159f9bd917320cf354fd816f885f3d088660cb
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-05-29 20:55:29 +0100

    Display images from the folder

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+  <title>Laravel Multiple File Upload Example</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4>Gallery</h4>
+                </div>
+ 
+                <div class="panel-body">
+                    <ul>
+                        @foreach($folders as $folder)
+                            <li> <a href="{{ URL::to('/display/Test28052019') }}">{{ $folder->imageDir }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
