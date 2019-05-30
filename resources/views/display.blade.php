8e412bf496a33cc58db5a5f008eadac7cbf64b65
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-05-30 22:33:52 +0100

    Added carousel for gallery images

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -19,6 +19,42 @@
                 <div class="panel-body">
                     <h3>Listing Images</h3>
  
+                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            @foreach( $images as $image )
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="{{url($image->image)}}" width="400" height="400" alt="Test">
+                                </div>
+                            @endforeach
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carouselExampleFade" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#carouselExampleFade" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4>Upload Multiple Images</h4>
+                </div>
+ 
+                <div class="panel-body">
+                    <h3>Listing Images</h3>
+ 
                     @forelse($images as $image)
                         <div class="col-md-4">
                             <img src="{{url($image->image)}}" width="300" height="200" class="img-responsive">
