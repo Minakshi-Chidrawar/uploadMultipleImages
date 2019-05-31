039eade57281736a2aa6a79e5e04fb7ada4e1544
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-05-31 23:08:19 +0100

    Amended to slide images

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -1,71 +1,48 @@
 <!DOCTYPE html>
-<head>
-  <title>Laravel Multiple File Upload Example</title>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-</head>
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4>Upload Multiple Images</h4>
-                </div>
- 
-                <div class="panel-body">
-                    <h3>Listing Images</h3>
- 
-                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner">
-                            @foreach( $images as $image )
-                                <div class="carousel-item">
-                                    <img class="d-block w-100" src="{{url($image->image)}}" width="400" height="400" alt="Test">
-                                </div>
-                            @endforeach
+<html lang="en">
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <style>
+            /* Make the image fully responsive */
+            .carousel-inner img {
+                width: 100%;
+                height: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">        
+                    <ol class="carousel-indicators">
+                    @foreach( $images as $image )
+                        <li data-target="#carouselExampleIndicators" data-slide-to="{{ $loop->index }}" class="{{ $loop->first ? 'active' : '' }}"></li>
+                    @endforeach
+                    </ol>
+                    
+                    <div class="carousel-inner" role="listbox">
+                        @foreach( $images as $image )
+                        <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
+                            <img class="img-responsive w-100" src="{{ $image->image }}" height="300" alt="Test">
                         </div>
-
-                        <!-- Controls -->
-                        <a class="left carousel-control" href="#carouselExampleFade" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#carouselExampleFade" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+                        @endforeach
                     </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4>Upload Multiple Images</h4>
-                </div>
- 
-                <div class="panel-body">
-                    <h3>Listing Images</h3>
- 
-                    @forelse($images as $image)
-                        <div class="col-md-4">
-                            <img src="{{url($image->image)}}" width="300" height="200" class="img-responsive">
-                        </div>
-                    @empty
-                        No image found
-                    @endforelse
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</body>
+    </body>
 </html>
